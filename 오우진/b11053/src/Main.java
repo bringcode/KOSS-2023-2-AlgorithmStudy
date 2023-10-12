5602c9a04233cfa9a5d93a5c97b8bdf6a248f7b5
bringcode <79398104+bringcode@users.noreply.github.com>
2023-10-12 15:59:04 +0900

    주석추가

--- 오우진/b11053/src/Main.java
+++ 오우진/b11053/src/Main.java
@@ -15,21 +15,26 @@ public class Main {
         // 배열 선언
         int[] arr = new int[n];
 
+        // 입력받은 배열 문자열을 토큰별로 나누고 이를 arr 배열에 추가
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
-
+        // findLongest 메서드를 이용해 가장 긴 수열 찾음
         int counter = findLongest(arr, n);
         System.out.printf(String.valueOf(counter));
     }
 
+    //dp를 이용한 가장 긴 수열 찾는 메서드
     public static int findLongest(int[] arr, int n) {
         int[] dp = new int[n];
+        // 수열의 길이와 가장 긴 수열의 길이를 담을 변수
         int max_length, length;
 
+        // 가장 긴 수열의 길이를 찾을 인덱스(ind)를 지정
         for (int ind = 0; ind < n; ind++) {
             max_length = 1;
+            // 가장 긴 수열의 길이를 찾을 인덱스보다 더 전에 위치한 수 중 arr[ind]보다 더 작은 수를 찾고, 그것을 이용해 dp 업데이트
             for (int i = 0; i < ind; i++) {
                 if (arr[ind] > arr[i]) {
                     length = dp[i] + 1;
@@ -41,9 +46,11 @@ public class Main {
             dp[ind] = max_length;
         }
 
+        // dp 배열에서 가장 큰 값 찾기
         return findMax(dp, n);
     }
 
+    // 더 큰 값 찾는 메서드
     public static int findMax(int[] dp, int n) {
         int maxLength = 1;
         for (int i = 0; i < n; i++) {
